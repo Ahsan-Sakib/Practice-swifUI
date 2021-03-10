d7d92d300f82ac38e9f5eb0c86b7a0cd5a6a924a
Ahsanul Kabir <ahsanul.kabir@bjitgroup.com>
2021-03-10 12:09:07 +0600

    [ios]swiftUI ui-element

---
+++ ContentView.swift
@@ -0,0 +1,56 @@
+//
+//  ContentView.swift
+//  UI-Elements
+//
+//  Created by Ahsanul Kabir on 10/3/21.
+//  Copyright © 2021 sakibwrold. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack{
+            Spacer()
+            HStack{
+                Spacer()
+                Image("lemon")
+                Spacer()
+                Image("lemon")
+                Spacer()
+                Image("lemon")
+                Spacer()
+
+            }
+            Spacer()
+            HStack{
+                Spacer()
+                Image("lemon")
+                Spacer()
+                Image("lemon")
+                Spacer()
+                Image("lemon")
+                Spacer()
+
+            }
+            Spacer()
+            HStack{
+                Spacer()
+                Image("lemon")
+                Spacer()
+                Image("lemon")
+                Spacer()
+                Image("lemon")
+                Spacer()
+
+            }
+            Spacer()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
